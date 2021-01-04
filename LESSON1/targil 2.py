10f2022c920485e0a2c3d01c38acf18528eac48b
lior74 <lior747474@gmail.com>
2021-01-04 16:25:18 +0200

    loooo

---
+++ LESSON1/targil 2.py
@@ -0,0 +1,10 @@
+name=input("Enter your name: ")
+age=28
+mail=input("Enter your mail: ")
+print(mail)
+print(type(mail))
+
+
+
+
+print("Hello " + name + "\nHow are you?\nYour age is: " + str(age))
