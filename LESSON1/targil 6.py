86fb089b79611a15684c948b6d514c8418f4d8e5
lior74 <lior747474@gmail.com>
2021-01-06 09:52:31 +0200

    str

---
+++ LESSON1/targil 6.py
@@ -0,0 +1,17 @@
+from time import sleep
+
+name=input("Enter your name: \n")
+if(name=="idan"):
+    print("Hello idan\n")
+elif(name=="ben"):
+    print("Hello ben\n")
+elif(name=="igal"):
+    print("Hello igal\n")
+else:
+    print("Where is my friends?...\n")
+
+print("it was a nice game, bye bye\n")
+
+
+
+
